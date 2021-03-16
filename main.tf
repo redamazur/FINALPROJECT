50291cb02e36bdbf04c7c3c5f85c155265aa462e
rado <rado@radoua.fr>
2021-03-16 08:08:10 +0000

    ip & subnet

--- main.tf
+++ main.tf
@@ -15,16 +15,37 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name = "RG_DEV2"
-  location = "westus"
+  location = "westeurope"
 }
 
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnetDEV"
+resource "azurerm_virtual_network" "Final_Network" {
+    name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
-    location            = "westus"
+    location            = "westeurope"
 #    resource_group_name = "RG_DEV2"
     resource_group_name = azurerm_resource_group.rg.name
     tags = {
         environment = "Terraform Demo"
     }
 }
+
+resource "azurerm_subnet" "Dev-Subnet"{
+    name                 = "myVnet"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.Final_Network.name
+    address_prefixes       = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "Dev_publicip" {
+    name                         = "myPublicIP"
+    location                     = "westeurope"
+    resource_group_name          = azurerm_resource_group.rg.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+
+
