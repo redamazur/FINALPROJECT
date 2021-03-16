0b2cb34b562d5326b6a52b4c6e4c76e40f0d4e2e
rado <rado@radoua.fr>
2021-03-16 09:05:21 +0000

    VM+IP+SRG+NIC

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "rg_Dev" {
 
 resource "azurerm_virtual_network" "Final_Network" {
     name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
+    address_space       = ["192.168.0.0/16"]
     location            = azurerm_resource_group.rg_Dev.location
     resource_group_name = azurerm_resource_group.rg_Dev.name
     tags = {
@@ -28,15 +28,15 @@ resource "azurerm_virtual_network" "Final_Network" {
     }
 }
 
-resource "azurerm_subnet" "Dev-Subnet"{
+resource "azurerm_subnet" "Dev_Subnet"{
     name                 = "myVnet"
     resource_group_name  = azurerm_resource_group.rg_Dev.name
     virtual_network_name = azurerm_virtual_network.Final_Network.name
-    address_prefixes       = ["10.0.2.0/24"]
+    address_prefixes       = ["192.168.1.0/24"]
 }
 
 resource "azurerm_public_ip" "Dev_publicip" {
-    name                 = "myPublicIP"
+    name                 = "Dev_PublicIP"
     location             = azurerm_resource_group.rg_Dev.location
     resource_group_name  = azurerm_resource_group.rg_Dev.name
     allocation_method    = "Dynamic"
@@ -46,5 +46,61 @@ resource "azurerm_public_ip" "Dev_publicip" {
     }
 }
 
+resource "azurerm_network_security_group" "web_dev_nsg" {
+    name                = "web_dev_NetworkSecurityGroup"
+    location            = azurerm_resource_group.rg_Dev.location
+    resource_group_name = azurerm_resource_group.rg_Dev.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "http"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_network_interface" "web_Dev_01_nic" {
+    name                        = "web_Dev_01_nic"
+    location                    = azurerm_resource_group.rg_Dev.location
+    resource_group_name         = azurerm_resource_group.rg_Dev.name
+
+    ip_configuration {
+        name                          	= "web_Dev_01_nic_Configuration"
+        subnet_id                     	= azurerm_subnet.Dev_Subnet.id
+        private_ip_address_allocation 	= "static"
+	private_ip_address 		= "192.168.1.10"
+        public_ip_address_id          	= azurerm_public_ip.Dev_publicip.id
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id      = azurerm_network_interface.web_Dev_01_nic.id
+    network_security_group_id = azurerm_network_security_group.web_dev_nsg.id
+}
 
 
