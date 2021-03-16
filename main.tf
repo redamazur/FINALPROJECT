b439f4716ff86bb1df8831b3e32c27e71540bf3b
rado <rado@radoua.fr>
2021-03-16 08:32:21 +0000

    changement var RG en RG_Dev

--- main.tf
+++ main.tf
@@ -13,16 +13,16 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "rg" {
-  name = "FINAL_PROJECT_EP_RB" 
+resource "azurerm_resource_group" "rg_Dev" {
+  name = "FINAL_PROJECT_EP_RB_Dev" 
   location = "westeurope"
 }
 
 resource "azurerm_virtual_network" "Final_Network" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg_Dev.location
+    resource_group_name = azurerm_resource_group.rg_Dev.name
     tags = {
         environment = "Terraform Demo"
     }
@@ -30,15 +30,15 @@ resource "azurerm_virtual_network" "Final_Network" {
 
 resource "azurerm_subnet" "Dev-Subnet"{
     name                 = "myVnet"
-    resource_group_name  = azurerm_resource_group.rg.name
+    resource_group_name  = azurerm_resource_group.rg_Dev.name
     virtual_network_name = azurerm_virtual_network.Final_Network.name
     address_prefixes       = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "Dev_publicip" {
     name                 = "myPublicIP"
-    location             = azurerm_resource_group.rg.location
-    resource_group_name  = azurerm_resource_group.rg.name
+    location             = azurerm_resource_group.rg_Dev.location
+    resource_group_name  = azurerm_resource_group.rg_Dev.name
     allocation_method    = "Dynamic"
 
     tags = {
