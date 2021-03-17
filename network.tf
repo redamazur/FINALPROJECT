a59bad16a27760b1998c31a028cc91b4a40c9ed7
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-17 23:18:32 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@ data "azurerm_virtual_network" "Final_Network" {
 }
 
 resource "azurerm_subnet" "Dev_Subnet"{
-    name                 = "deploySubnet"
+    name                 = "Dev_Subnet"
     resource_group_name  = data.azurerm_resource_group.rg_Dev.name
     virtual_network_name = data.azurerm_virtual_network.Final_Network.name
     address_prefixes       = ["192.168.1.0/24"]
