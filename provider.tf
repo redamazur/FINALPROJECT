7192517719547d1bfb4a2bc6e71073af7088bb59
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-17 21:58:41 +0100

    Update provider.tf
    add meta data

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 provider "azurerm" {
   # Configuration options
-  features {}
+  features {
+    appId: 0b639f67-04d2-43d6-97a7-a29446318a9e
+		displayName: azure-cli-2021-03-17-12-17-22
+		name: http://azure-cli-2021-03-17-12-17-22
+		password: 3COqYkTEJWRj11jkvYGojJCNh.RjnVNGIB
+		tenant: ed0e3414-6e56-454c-8a82-eb80befb738b
+    resource_group_name   = azurerm_resource_group.main.name
+    }
 }
