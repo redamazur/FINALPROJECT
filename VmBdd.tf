52793e52cc82040db6a2bf325dd8fc24eaaf60be
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-17 22:53:52 +0100

    Update VmBdd.tf

--- VmBdd.tf
+++ VmBdd.tf
@@ -1,7 +1,7 @@
 resource "azurerm_linux_virtual_machine" "Dev-Bdd-vm" {
     name                  = "Dev-Bdd-VM"
-    location              = azurerm_resource_group.rg_Dev.location
-    resource_group_name   = azurerm_resource_group.rg_Dev.name
+    location              = data.azurerm_resource_group.rg_Dev.location
+    resource_group_name   = data.azurerm_resource_group.rg_Dev.name
     network_interface_ids = [azurerm_network_interface.Dev_Bdd_01_nic.id]
     size                  = "Standard_B1ms"
 
