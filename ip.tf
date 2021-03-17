d0568d1a98c9eb7e3897a9cc0cff9e27a3976fc7
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-17 22:53:27 +0100

    Update ip.tf

--- ip.tf
+++ ip.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "Dev_publicip" {
     name                 = "Dev_PublicIP"
-    location             = azurerm_resource_group.rg_Dev.location
-    resource_group_name  = azurerm_resource_group.rg_Dev.name
+    location             = data.azurerm_resource_group.rg_Dev.location
+    resource_group_name  = data.azurerm_resource_group.rg_Dev.name
     allocation_method    = "Dynamic"
 
     tags = {
