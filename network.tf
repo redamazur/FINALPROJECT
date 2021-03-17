fcfbbc2ae1dfff114625422472a8cf7e844ca815
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-17 22:53:06 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "Final_Network" {
     name                = "myVnet"
     address_space       = ["192.168.0.0/16"]
-    location            = azurerm_resource_group.rg_Dev.location
-    resource_group_name = azurerm_resource_group.rg_Dev.name
+    location            = data.azurerm_resource_group.rg_Dev.location
+    resource_group_name = data.azurerm_resource_group.rg_Dev.name
     tags = {
         environment = "Terraform Demo"
     }
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "Final_Network" {
 
 resource "azurerm_subnet" "Dev_Subnet"{
     name                 = "deploySubnet"
-    resource_group_name  = azurerm_resource_group.rg_Dev.name
-    virtual_network_name = azurerm_virtual_network.Final_Network.name
+    resource_group_name  = data.azurerm_resource_group.rg_Dev.name
+    virtual_network_name = data.azurerm_virtual_network.Final_Network.name
     address_prefixes       = ["192.168.1.0/24"]
 }
