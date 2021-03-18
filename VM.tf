48c144032ab0f713fdfcf4f0f6b80840ff1b282b
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-18 15:11:05 +0100

    Update VM.tf

--- VM.tf
+++ VM.tf
@@ -37,11 +37,6 @@ resource "azurerm_linux_virtual_machine" "Dev-Web-vm" {
     }
 
 
-
-#    boot_diagnostics {
-#        storage_account_uri = azurerm_storage_account.Dev_Storage.primary_blob_endpoint
-#    }
-
     tags = {
         environment = "Terraform Demo"
     }
