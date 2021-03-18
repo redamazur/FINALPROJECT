0d41028973ebc47c3ca06fb56043d1891f49746a
redamazur <77279486+redamazur@users.noreply.github.com>
2021-03-18 15:24:39 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -8,3 +8,9 @@ data "azurerm_subnet" "Dev_Subnet" {
     resource_group_name  = data.azurerm_resource_group.rg_Dev.name
     virtual_network_name = data.azurerm_virtual_network.Final_Network.name 
 }
+
+data "azurerm_subnet" "Prod_Subnet" {
+    name                = "deployProdsubnet"
+    resource_group_name  = data.azurerm_resource_group.rg_Dev.name
+    virtual_network_name = data.azurerm_virtual_network.Final_Network.name 
+}
